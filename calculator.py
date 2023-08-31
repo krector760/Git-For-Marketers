33de819b58fd11c257cda3618f979127be623573
Kyle <krector760@gmail.com>
2023-08-31 08:15:08 -0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,26 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero"
+
+def exponentiate(a, b):
+    return a ** b
+
+num1 = 10
+num2 = 5
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
+print("Exponentiation:", exponentiate(num1, num2))
